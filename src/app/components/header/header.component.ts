115ea2803d1b341b652d462ded653eee3a3a2a39
Gabo1794 <sonic1794@gmail.com>
2020-06-06 13:42:17 -0500

    se versiona primer proyecto de angular

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    //template: ``, sirve para poner poco codigo html
+    templateUrl: './header.component.html', // sirve para poder importar un componente
+}) 
+export class HeaderComponent {
+
+}
