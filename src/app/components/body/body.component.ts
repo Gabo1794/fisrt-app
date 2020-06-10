e1f045dbfcc89d58451d40aa359097a3eafd6d44
Raúl Gabriel Montaño Macías <gabriel.montano@brivesoluciones.com>
2020-06-09 20:07:40 -0500

    se finaliza proyecto 1 de angular

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,5 +5,12 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent{
-    
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    }
+
+    mostrar = true;
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Logan'];
 }
